5b57ff9ab6b1690329330a831a2cf036d0781783
Phylloxera <adickey7@hotmail.com>
2024-08-30 13:54:48 -0500

    Fix double comma in skesa plasmid annotation

--- singfiles/metaclean.R
+++ singfiles/metaclean.R
@@ -3,4 +3,6 @@ mydf<-read.delim("metadata.txt", row.names = NULL)
 mydf <- mydf[,colSums(is.na(mydf))<nrow(mydf)]
 cols_to_be_rectified <- names(mydf)[vapply(mydf, is.character, logical(1))]
 mydf[, cols_to_be_rectified] <- lapply(mydf[, cols_to_be_rectified], trimws)
+mydf[] <- lapply(mydf, sub, pattern = ",,", replacement = ",") #7/10/24 double commas in SkesaPlasList
+mydf[] <- lapply(mydf, sub, pattern = ",$", replacement = "") #7/10/24 trailing commas in SkesaPlasList
 write.table(mydf, "metadata.txt", sep="\t", na="", quote=F, row.names=F)
